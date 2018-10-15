72c2db1be673bd58dd3642e976589edfbaa6a533
nidharaisa <39795332+nidharaisa@users.noreply.github.com>
2018-10-15 17:45:51 +0600

    different TURN server

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ const configuration = {
     urls: 'stun:stun.l.google.com:19302'
     },
     {
-    url: 'turn:turn.bistri.com:80',
-    credential: 'homeo',
-    username: 'homeo'
+    urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+    credential: 'webrtc',
+    username: 'webrtc'
     }
   ]
 };
